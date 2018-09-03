62ac9993e5d2c27f8e2fde9bb6dadbf54f593801
tapigami <tapigami@icloud.com>
2018-09-04 07:36:56 +0900

    Init by xcode

---
+++ numCounterIos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  numCounterIos
+//
+//  Created by 西上 良祐 on 2018/09/04.
+//  Copyright © 2018年 tapigami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
